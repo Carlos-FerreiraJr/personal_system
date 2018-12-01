61c9a2a7b57e3c02a3733dd12a85f5fd9dc6f756
Carlos <carlosferreiratech@gmail.com>
2018-12-01 01:04:22 -0200

    whatsapp api added

--- class/sql.php
+++ class/sql.php
@@ -3,22 +3,25 @@
 class sql extends PDO{
     private $conn;
     function __construct(){
-        $this->conn = new PDO("mysql:dbname=<dbname>;host=<localhost>","<usuario>","<senha>");/*TODO DADOS DE CONEXAO ENTRE < >*/
+        $this->conn = new PDO("mysql:dbname=testes;host=localhost","root","");/*TODO DADOS DE CONEXAO ENTRE < >*/
     }
 
 //funcao de selecionar os dados 
     function select($idcod){
-        $stmt = $this->conn->prepare("select * from clientes where <tabela> = ? ");/*TODO dado da tabela */
+        $stmt = $this->conn->prepare("select * from clientes where vendedor = ? ");/*TODO dado da tabela */
         $codigo = $idcod;
         $stmt->execute([$codigo]);
 
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         foreach ($result as $resultados) {
+             $resultados["telefone"] = "<a href=https://wa.me/55" . $resultados["telefone"] . "?text=credito%20nao%20aprovado >" .$resultados["telefone"] . "</a>"; 
             foreach ($resultados as $key => $value) {
-            echo "<i style='color:red'>" . $key ."</i> ::" . $value . "</br>";
+            echo "<i style='color:red'>" . $key ."</i> ::" . $value .   "</br>";
             }
             echo "<br>";
             echo "<br>";
         }
+
     }   
 }
