bf2edff0c9b79761935a7089c50b5d327b9660f0
Carlos <carlosferreiratech@gmail.com>
2018-12-02 15:29:20 -0200

    register added

--- class/sql.php
+++ class/sql.php
@@ -1,32 +1,33 @@
 <?php 
-//classe de conexao 
-class sql extends PDO{
-    private $conn;
-    function __construct(){
-        $this->conn = new PDO("mysql:dbname=testes;host=localhost","root","");/*TODO DADOS DE CONEXAO ENTRE < >*/
-    }
+    /* ######################################classe sql #################################### */
+        class sql extends PDO{
+            private $conn;
 
-//funcao de selecionar os dados 
-    function select($idcod){
-        $stmt = $this->conn->prepare("select * from clientes where vendedor = ? ");/*TODO dado da tabela */
-        $codigo = $idcod;
-        $stmt->execute([$codigo]);
+            function __construct(){
+                $this->conn = new PDO("mysql:dbname=testes;host=localhost","root","");/*TODO DADOS DE CONEXAO ENTRE < >*/
+            }
 
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            function select($idcod){
+                $stmt = $this->conn->prepare("select * from clientes where vendedor = ? ");/*TODO dado da tabela */
+                $codigo = $idcod;
+                $stmt->execute([$codigo]);
 
-        foreach ($result as $resultados) {
-            $ruptura = $resultados["ruptura"];
-            $ruptura = preg_replace('/\s+/','%20',$ruptura);
-            $resultados["telefone"] = "<a href=https://wa.me/55" . $resultados["telefone"] . "?text=$ruptura>" .$resultados["telefone"] . "</a>"; 
-            echo "<div class='list-group col-md-10'>".
-            "<p class='list-group-item list-group-item-action bg-info' id='resolver'>". $resultados["orders"] . "</p>" . 
-            "<p class='list-group-item list-group-item-action'>". $resultados["nome"] . "</p>".
-            "<p class='list-group-item list-group-item-action'>" .$resultados["ruptura"] ."</p>".
-            "<p class='list-group-item list-group-item-action'>" .$resultados["telefone"] ."</p>".
-            "<p class='list-group-item list-group-item-action' id='botaoresolver'>" ."<input class='btn btn-warning type='button' placeholder='resolver' >"."</p>".
-          "</div>". 
-          "<br>";
-        
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                foreach ($result as $resultados) {
+                    $ruptura = $resultados["ruptura"];
+                    $ruptura = preg_replace('/\s+/','%20',$ruptura);
+                    $resultados["telefone"] = "<a href=https://wa.me/55" . $resultados["telefone"] . "?text=$ruptura>" .$resultados["telefone"] . "</a>"; 
+                    echo "<div class='list-group col-md-10'>".
+                    "<p class='list-group-item list-group-item-action bg-info' id='resolver'>". $resultados["orders"] . "</p>" . 
+                    "<p class='list-group-item list-group-item-action'>". $resultados["nome"] . "</p>".
+                    "<p class='list-group-item list-group-item-action'>" .$resultados["ruptura"] ."</p>".
+                    "<p class='list-group-item list-group-item-action'>" .$resultados["telefone"] ."</p>".
+                    "<p class='list-group-item list-group-item-action' id='botaoresolver'>" ."<input class='btn btn-warning type='button' placeholder='resolver' >"."</p>".
+                    "</div>". 
+                    "<br>";
+                }   
+            }
+    
         }
-    }   
-}
+    /* ##################################################################################### */
--- pedidos.php
+++ pedidos.php
@@ -6,72 +6,100 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="styles.css" rel="stylesheet">
-
-    <title>Document</title>
+    <title>Pedidos</title>
 </head>
 <body>
-    
-<div>
-      <?php require_once('nav.php')?>
-<div>
+
+    <!-- ######################################requisicao da nav################################# -->
+        <div>
+            <?php require_once('nav.php')?>
+        </div>
+    <!-- ########################################################################################### -->
 
     <br>  
     <br> 
 
-
 <center>  
 
-    <section class="container-fluid">
-    <div class="card border-sencondary title-card">
-        <h3>Consulta de rupturas</h3> 
-    <div>
-     <!-- formulario de consulta -->
-    <div class="card-body">
-    <form action="" method="post">
-        <select name ="vendedor" class="custom-select custom-select-lg mb-3 border-sencondary">
-            <option selected value="0">selecione um</option>
-            <option value="1">herlander</option>
-            <option value="2">Victor</option>
-        </select>
-        <button class="btn btn-" type="submit">Selecionar</button>
-        <form>
-    <div>
-    </section>
+    <!-- #############################insercao e consulta de informacao############################## -->
+   
+        <section class="container-fluid">
 
-    <section class="container-fluid">
+            <!-- --- ------------------area de botoes --------------------------- -->
+                <p>
+                    <button class="btn btn-primary" data-toggle="collapse"data-target="#cadastrar" role="button" aria-expanded="false" >
+                        Cadastrar
+                    </button>
+                    <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#consulta" aria-expanded="false" >
+                    Consultar
+                    </button>
+                </p>
+            <!-- ________________________________________________________________ -->
+
+            
+            
+            <!-- --- ------------------area de cadastro --------------------------- -->
+            <div class="collapse" id="cadastrar">
+                <div class="card card-body">
+                   
+                </div>
+            </div>
+            <!-- __________________________________________________________________ -->
+
+
+
+            <!-- --- ------------------area de consulta --------------------------- -->
+                <div class="collapse" id="consulta">
+                    <div class="card border-sencondary title-card">
+                        <h3>Consulta de rupturas</h3> 
+                        <form action="" method="post">
+                            <select name ="vendedor" class="custom-select custom-select-lg mb-3 border-sencondary">
+                                <option selected value="0">selecione um</option>
+                                <option value="1">herlander</option>
+                                <option value="2">Victor</option>
+                            </select>
+                            <button class="btn btn-" type="submit">Selecionar</button>
+                        <form>
+                    </div>
+                </div>
+            <!-- __________________________________________________________________ -->
+
+        </section>
+
+    <!-- ############################################################################################ -->
+
     <br>
     <br>
-     <!-- progresso nas rupturas -->
 
-    <!-- <div class="progress">
-        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-    </div> -->
+    <!-- ################################secao de envio para mysql#################################### -->
+        <section class="container-fluid">
 
-    <?php  
-
-        //chamada da classe sql
-        //-------------------------------------------------   
-        require("configs.php");
-        $teste = new sql();
-        if (isset($_POST["vendedor"])) {
-            if ($_POST["vendedor"]==0){
-                echo "<p style='color:red'>voce precisa escolher um vendedor<p>";    
-            }else{
+            <!-- --- ------------------chamada sql --------------------------- -->
+            <?php    
                 require("configs.php");
                 $teste = new sql();
-                $teste->select($_POST["vendedor"]);
-            }
-        }
-            //-------------------------------------------------
-    ?>
-    
-     <?php require_once('scripts.php')?>
-    </section>
+                if (isset($_POST["vendedor"])) {
+                    if ($_POST["vendedor"]==0){
+                        echo "<p style='color:red'>voce precisa escolher um vendedor<p>";    
+                    }else{
+                        require("configs.php");
+                        $teste = new sql();
+                        $teste->select($_POST["vendedor"]);
+                    }
+                }
+            ?>
+            <!-- __________________________________________________________________ -->
+        </section>
+    <!-- ############################################################################################# -->
+
+    <br>
+    <br>
+
+    <!-- ##################################chamada de scripts######################################### -->
+                <?php require_once('scripts.php')?>
+    <!-- ############################################################################################# -->
+
 </center>
-
-
-
-
 
 
 <script>
@@ -79,20 +107,20 @@
     resolver = document.querySelectorAll('#resolver');
     botao = document.querySelectorAll('#botaoresolver');
 
-for (let i = 0; i < resolver.length; i++) {
-    $(botao[i]).click(function(){
-    if($(resolver[i]).hasClass('bg-info')){
-        $(resolver[i]).siblings().addClass('greenalpha');
-        $(resolver[i]).removeClass('bg-info');
-        $(resolver[i]).addClass('bg-success');
-    }else{
-        $(resolver[i]).siblings().removeClass('greenalpha');
-        $(resolver[i]).removeClass('bg-success');
-        $(resolver[i]).addClass('bg-info');
-    }
-    });
-}
-  
+    for (let i = 0; i < resolver.length; i++) {
+            $(botao[i]).click(function(){
+            if($(resolver[i]).hasClass('bg-info')){
+                $(resolver[i]).siblings().addClass('greenalpha');
+                $(resolver[i]).removeClass('bg-info');
+                $(resolver[i]).addClass('bg-success');
+            }else{
+                $(resolver[i]).siblings().removeClass('greenalpha');
+                $(resolver[i]).removeClass('bg-success');
+                $(resolver[i]).addClass('bg-info');
+            }
+        });
+    }  
 </script>
+
 </body>
 </html>
